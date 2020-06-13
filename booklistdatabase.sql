336c2be0c78d28f19fe0b8a7cc06d0d6ba9595c5
Alan Zhou <54216387+AlanReviews@users.noreply.github.com>
2020-06-13 17:03:24 -0400

    Add files via upload

---
+++ booklistdatabase.sql
@@ -0,0 +1,7 @@
+CREATE TABLE books (
+id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+Insert INTO books VALUES (1, "Divergent", 9);
+Insert INTO books VALUES (2, "Fahrenheit 451", 9);
+Insert INTO books VALUES (3, "The Limit", 10);
+Insert INTO books VALUES (4, "Harry Potter and The Goblet of Fire", 9.5);
+SELECT * FROM books;
